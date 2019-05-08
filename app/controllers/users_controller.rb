4542b5c5dde64587214ffdb7de91c18df472698d
ruby <tranngochhieu.sqtt@gmail.com>
2019-05-08 08:59:08 +0700

    sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,5 +6,23 @@ class UsersController < ApplicationController
   end
 
   def new
+    @user = User.new
+  end
+
+  def create 
+    @user = User.new(user_params)
+    if @user.save 
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+      else
+        render 'new'
+      end
+      private
+      def user_parmas
+        params.require(:user).permit(:name, :email, :password,
+          :password_confirmation)
+
+        
+      end
   end
 end
